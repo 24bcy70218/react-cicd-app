cbda0c67b2ea11964d0414d9c79f173e396ec127
24bcy70218 <24BCY70218@cuchd.in>
2026-04-23 22:06:16 +0530

    Add files via upload

---
+++ react-cicd-app/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div style={{textAlign:"center",marginTop:"50px"}}>
+      <h1>CI/CD React App 🚀</h1>
+      <p>GitHub Actions + Docker + GHCR</p>
+    </div>
+  );
+}
+export default App;
